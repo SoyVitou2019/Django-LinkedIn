9f288615eff35c98e15eff50f8dd6ba16aa7ef45
Soy Vitou Pro <118577815+SoyVitou2019@users.noreply.github.com>
2024-01-16 13:09:57 +0700

    return html

--- home/views.py
+++ home/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse("Hello, world!")
+
+# render html
 def home(request):
-    return HttpResponse("Hello, world!")
+    return render(request, './home/index.html')
+
